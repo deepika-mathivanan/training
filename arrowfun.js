5405121d47496102a328dbfacda8c48f84ff552a
Deepika_Mathivanan <deepikapm.22cse@kongu.edu>
2024-12-28 22:02:20 +0530

    day2 task

--- arrowfun.js
+++ arrowfun.js
@@ -2,3 +2,15 @@ arrowfun =(studentdept) => {
     return studentdept
 }
 console.log(arrowfun('aiml,cse',"aiml","it"))
+
+//without parameter
+arrowfun = () => {
+  console.log( "Hi this is deepika" );
+}
+arrowfun();
+
+//with parameter
+arrowfun = (name) => console.log("hello",name);
+arrowfun("deepika"); 
+
+
